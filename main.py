3823af4cdba620d2565b073190f4be54a6b2a5ac
Froxzen <harvardchong1@gmail.com>
2024-11-30 17:58:24 +1100

    Converted venue and opponenet to numerical values

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 matches = pd.read_csv("matches.csv", index_col = 0)
 
-matches.head()
+matches["date"] = pd.to_datetime(matches["date"])
+
+matches["venue_code"] = matches["venue"].astype("category").cat.codes
+
+matches["opp_code"] = matches["opponent"].astype("category").cat.codes
+
+print(matches)
