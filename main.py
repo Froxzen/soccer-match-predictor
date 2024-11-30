63a5b5a8be26254baea17fc0ed9c81a3b978824c
Froxzen <harvardchong1@gmail.com>
2024-11-30 18:07:22 +1100

    Cleaned more data

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ matches["date"] = pd.to_datetime(matches["date"])
 matches["venue_code"] = matches["venue"].astype("category").cat.codes
 
 matches["opp_code"] = matches["opponent"].astype("category").cat.codes
+matches["hour"] = matches["time"].str.replace(":.+", "", regex=True).astype("int")
+matches["day_code"] = matches["date"].dt.dayofweek
 
 print(matches)
